00576ea1f01684ca344ea6328e3dd2ef71c339d3
Archer <fenrisulfr.33@protonmail.com>
2025-08-28 12:09:52 -0600

    init commit

---
+++ exercises/Exercise1.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Exercise1 {
+    // Print the first element in the command line
+    public static void main(String[] args) {
+        System.out.println(args[0].charAt(0));
+    }
+}
