ad0765a8d8b13c128f53ee08fec204a4bb13e11e
Archer <fenrisulfr.33@protonmail.com>
2025-09-07 12:52:03 -0600

    Exercise 6 tests complete

--- exercises/Exercise6.java
+++ exercises/Exercise6.java
@@ -1,6 +1,5 @@
 package cs250.exercises;
 
-import java.sql.Array;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -22,28 +21,59 @@ public class Exercise6 {
      * @param args
      */
     public static void main(String[] args) {
+        // Create a string of all chars to random pull from
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         // Initialize a new random class
         Random random = new Random();
         // Using 3 arguments from the command line where the first is the type of number
         // second is x, the amount of columns, and y the amount of rows
         // create the appropriate sized matrix of random numbers
         String typeOfNumber = args[0];
-        int rows = Integer.parseInt(args[1]); // Number of rows
-        int cols = Integer.parseInt(args[2]); // Number of columns
+        int x = Integer.parseInt(args[1]); //
+        int y = Integer.parseInt(args[2]); //
         // if else for the three types on inputs
-        // Print out the array af random values after creation
+        // Print out the array af random values after creation of a 4x3 2D array
         if (typeOfNumber.equals("int")) {
-            int[][] twoDimensionalArray = new int[rows][cols];
+            // int[][] twoDimensionalArray = new int[4][3];
+            int[][] twoDimensionalArray = { { random.nextInt(), random.nextInt(), random.nextInt() },
+                    { random.nextInt(), random.nextInt(), random.nextInt() },
+                    { random.nextInt(), random.nextInt(), random.nextInt() },
+                    { random.nextInt(), random.nextInt(), random.nextInt() } };
             System.out.println(Arrays.deepToString(twoDimensionalArray));
-            System.out.println(twoDimensionalArray[rows][cols]);
+            System.out.println(twoDimensionalArray[x][y]);
+            Arrays.sort(twoDimensionalArray[x - y]);
+            System.out.println(Arrays.toString(twoDimensionalArray[x - y]));
         } else if (typeOfNumber.equals("float")) {
-            float[][] twoDimensionalArray = new float[rows][cols];
+
+            // float[][] twoDimensionalArray = new float[4][3];
+            float[][] twoDimensionalArray = {
+                    { random.nextFloat(), random.nextFloat(), random.nextFloat() },
+                    { random.nextFloat(), random.nextFloat(), random.nextFloat() },
+                    { random.nextFloat(), random.nextFloat(), random.nextFloat() },
+                    { random.nextFloat(), random.nextFloat(), random.nextFloat() } };
             System.out.println(Arrays.deepToString(twoDimensionalArray));
-            System.out.println(twoDimensionalArray[rows][cols]);
+            System.out.println(twoDimensionalArray[x][y]);
+            Arrays.sort(twoDimensionalArray[x - y]);
+            System.out.println(Arrays.toString(twoDimensionalArray[x - y]));
         } else if (typeOfNumber.equals("char")) {
-            char[][] twoDimensionalArray = new char[rows][cols];
+            // char[][] twoDimensionalArray = new char[x][y];
+            char[][] twoDimensionalArray = {
+                    { alphabet.charAt(random.nextInt(0, 27)),
+                            alphabet.charAt(random.nextInt(0, 26)),
+                            alphabet.charAt(random.nextInt(0, 26)) },
+                    { alphabet.charAt(random.nextInt(0, 27)),
+                            alphabet.charAt(random.nextInt(0, 26)),
+                            alphabet.charAt(random.nextInt(0, 26)) },
+                    { alphabet.charAt(random.nextInt(0, 27)),
+                            alphabet.charAt(random.nextInt(0, 26)),
+                            alphabet.charAt(random.nextInt(0, 26)) },
+                    { alphabet.charAt(random.nextInt(0, 27)),
+                            alphabet.charAt(random.nextInt(0, 26)),
+                            alphabet.charAt(random.nextInt(0, 26)) } };
             System.out.println(Arrays.deepToString(twoDimensionalArray));
-            System.out.println(twoDimensionalArray[rows][cols]);
+            System.out.println(twoDimensionalArray[x][y]);
+            Arrays.sort(twoDimensionalArray[x - y]);
+            System.out.println(Arrays.toString(twoDimensionalArray[x - y]));
         }
     }
 }
